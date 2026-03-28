f81fd2b0eac93cacaa6feff95878c882c160effc
Abhiram Vaitla <abhiramvaitla7687@gmail.com>
2026-03-28 10:48:09 +0530

    Initial commit - Task Tracker CLI

---
+++ index.js
@@ -0,0 +1,39 @@
+const {
+    addTask,
+    updateTask,
+    deleteTask,
+    markStatus,
+    listTasks
+} = require("./lib/taskService");
+
+const args = process.argv.slice(2);
+const command = args[0];
+
+switch (command) {
+    case "add":
+        addTask(args.slice(1).join(" "));
+        break;
+    
+    case "update":
+        updateTask(args[1], args.slice(2).join(" "));
+        break;
+
+    case "delete":
+        deleteTask(args[1]);
+        break;
+
+    case "mark-in-progress":
+        markStatus(args[1], "in-progress");
+        break;
+
+    case "mark-done":
+        markStatus(args[1], "done");
+        break;
+    
+    case "list":
+        listTasks(args[1]);
+        break;
+
+    default:
+        console.log("Invalid command");
+}
